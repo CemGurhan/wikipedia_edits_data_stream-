6e70f5389e4ec056c87f47848e57932e674743f0
Cem Gurhan <cemg@live.co.uk>
2023-01-18 14:14:54 +0000

    added post to eventhub producer service

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use eventsource::reqwest::Client;
 use reqwest::Url;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let new_reqwest_client = reqwest::Client::new();
     let client = Client::new(Url::parse("https://stream.wikimedia.org/v2/stream/recentchange").unwrap());
     for event in client {
-        println!("{}", event.unwrap());
+        let event_unwrapped = event.unwrap();
+        let _result = new_reqwest_client.post("http://0.0.0.0:8080/production").body(event_unwrapped.data).send().await;
     }
 }
